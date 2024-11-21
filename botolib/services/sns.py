c05cc363e97b75e23595af8700792481cc2a4b84
Jun Ke <anewr@live.com>
2024-11-21 11:03:37 +0800

    Update sns publish method

--- botolib/services/sns.py
+++ botolib/services/sns.py
@@ -1,5 +1,5 @@
 import json
-from typing import Union
+from typing import Dict, Union
 from . import AWSService
 from ..utils.common import remove_none_values
 
@@ -20,17 +20,27 @@ class SNS(AWSService):
         response = self.client.get_topic_attributes(TopicArn=topic_arn)
         return response.get('Attributes')
     
-    def publish(self, topic_arn, message: Union[dict, str], subject = None, message_attributes = None):
+    def publish(self, topic_arn, message: Union[dict, str], subject = None, message_attributes:Dict[str, Union[str, bytes]] = None):
         if isinstance(message, dict):
             message = json.dumps(message)
         elif not isinstance(message, str):
             message = str(message)
 
+        msg_attrs = {}
+        if message_attributes is not None:
+            for k,v in message_attributes.items():
+                if isinstance(v, str):
+                    msg_attrs[k] = {"DataType": "String", "StringValue": v}
+                elif isinstance(v, str):
+                    msg_attrs[k] = {"DataType": "Binary", "BinaryValue": v}
+                else:
+                    msg_attrs[k] = {"DataType": "String", "StringValue": str(v)}
+
         req_params = remove_none_values({
             "TopicArn": topic_arn,
             "Message": message,
             "Subject": subject,
-            "MessageAttributes": message_attributes
+            "MessageAttributes": msg_attrs
         })
 
         return self.client.publish(**req_params)
