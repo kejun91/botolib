a890a94e45afe0f25b7b9a8a6dcab19ea35d7876
Jun Ke <anewr@live.com>
2024-12-02 14:57:36 +0800

    Add S3 put_object

--- botolib/services/s3.py
+++ botolib/services/s3.py
@@ -19,9 +19,19 @@ class S3(AWSService):
         self.client.download_fileobj(bucket_name,key,file_obj)
 
     def get_object(self, s3_path:str) -> bytes:
-        parts = s3_path.removeprefix('/').split('/',1)
-        bucket_name = parts[0]
-        key_name = parts[1] if len(parts) > 1 else ""
-
+        bucket_name, key_name = get_bucket_and_key(s3_path)
         response = self.client.get_object(Bucket=bucket_name, Key=key_name)
         return response["Body"].read()
+    
+    def put_object(self, s3_path:str, body:bytes|str, content_type:str = "application/json"):
+        bucket_name, key_name = get_bucket_and_key(s3_path)
+        if isinstance(body,str):
+            body = body.encode('utf-8')
+        self.client.put_object(Bucket=bucket_name, Key=key_name, Body=body,ContentType=content_type)
+
+def get_bucket_and_key(s3_path:str):
+    parts = s3_path.removeprefix('/').split('/',1)
+    bucket_name = parts[0]
+    key_name = parts[1] if len(parts) > 1 else ""
+
+    return bucket_name, key_name
