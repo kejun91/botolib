d302b69f25fcf197ebcdade734fe1c0f3ffc160d
Jun Ke <anewr@live.com>
2024-12-03 10:59:53 +0800

    Add IdentityStore list users method

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="botolib",
-    version="0.0.18",
+    version="0.0.19",
     author="Jun Ke",
     author_email="kejun91@gmail.com",
     description="A boto lib that enhances some aws service clients",
